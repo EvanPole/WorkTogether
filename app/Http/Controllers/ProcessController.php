765646c5895294237680f2d6d7ea1144a96d3acb
Evan <53783383+EvanPole@users.noreply.github.com>
2024-04-02 21:15:52 +0200

    add order model and process store stats code

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -2,28 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Rack;
+use App\Models\Order;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProcessController extends Controller
 {
     public function store(Request $request)
     {
-        // dd($request);
-        $validatedData = $request->validate([
-            'purchase_option' => 'required',
-            'custom_duration' => 'required|numeric|min:1',
-        ]);
 
-        $process = new Rack();
-        $process->service = $request->input('service');
-        $process->description = $request->input('desc');
-        $process->price = $request->input('price');
-        $process->purchase_option = $validatedData['purchase_option'];
-        $process->custom_duration = $validatedData['custom_duration'];
+        // $validatedData = $request->validate([
+        //     'purchase_option' => 'required',
+        //     'custom_duration' => 'required|numeric|min:1',
+        // ]);
 
+        $process = new Order();
+        $process->user_id = Auth::user()->id;
+        $process->rack_id = 1;
+        $process->price = 10;
+        $process->discount = "1";
+        $process->start_date = Carbon::now();
+        $process->end_date = Carbon::now();
         $process->save();
 
-        return redirect('/')->with('success', 'Processus enregistré avec succès.');
+        return view('dashboard.index');
     }
 }
