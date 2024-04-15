ba5890fd1fd3af1aaee052c5771b51869ad1cc66
Evan <53783383+EvanPole@users.noreply.github.com>
2024-04-15 12:09:01 +0200

    add address

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('lastname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->text('address');
             $table->integer('postalcode');
             $table->string('city');
             $table->string('country');
